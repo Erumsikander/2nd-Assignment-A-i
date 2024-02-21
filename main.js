7451821720356d85b14b163f88346c807999f241
Erum Sikander <mrsikanderghouri@gmail.com>
2024-02-22 04:22:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Class No 3rd: 20-02-2024 
+// Q1: How To use Var , let , Const?
+// Answer: 
+// var:
+var myname = "Erum";
+console.log(myname);
+// let:
+let favouritecolor = "Black";
+console.log(favouritecolor);
+// const:
+const favouritecar = "Revo";
+console.log(favouritecar);
